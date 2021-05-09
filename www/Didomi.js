9c509691f1491a0e82359aa34a858e1ebeaaf126
Benjamin Guedj <benjaminguedj@hotmail.fr>
2021-05-09 17:14:14 +0200

    add log

--- www/Didomi.js
+++ www/Didomi.js
@@ -1,5 +1,6 @@
 var exec = require('cordova/exec');
 
 exports.shareConsent = function (success, error) {
+    console.log("shareConsent : exec CDVDidomi - shareConsent");
     exec(success, error, 'CDVDidomi', 'shareConsent', []);
 };
